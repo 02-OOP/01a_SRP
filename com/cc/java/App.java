e33952e88262e73efe9ba64b3e43650db4ea1468
Peter <schmidt.multimedia@gmail.com>
2022-04-07 13:53:59 +0200

    update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,7 +4,32 @@ public class App {
     
     public static void main(String[] args) {
      
+        // Restaurant
+
+        // Gemüse putzen --> Gemüse kochen --> Teller anrichten --> Teller servieren
+
+        ausgabe(anrichten(kochen(putzen())));
+    
     }
+
+    public static String putzen() {
+        return "geputzen Brokkoli";
+    }
+
+    public static String kochen(String input) {
+        return "gekochten, " + input;
+    }
+
+    public static String anrichten(String input) {
+        return input + " auf einem Teller";
+    }
+
+    public static void ausgabe(String outputStr) {
+        System.out.println(outputStr);
+    }
+
+
+
 
 }
 
